1e8ef1c77d6e833c4ef9098490071c8f27629b10
Ernest <ernestez12@gmail.com>
2023-12-21 10:40:32 +0100

    Authentication Initialized

---
+++ readMe.md
@@ -0,0 +1,14 @@
+Stack Used:
+ Node.js (Backend)
+ Ejs (Templating Engine)
+ Bootstrap (Frontend)
+
+ Architecture
+   MVC
+ Database: MongoDB Atlas
+
+ To Use, 
+  Clone and Run npm Install,
+  Create config folder and Config.env  (config/config.env)
+ 
+
--- server.js
+++ server.js
@@ -6,12 +6,23 @@ const helmet = require("helmet");
 const cors = require("cors");
 const path = require("path")
 const expressLayouts = require("express-ejs-layouts");
-
+const connectDB = require("./server/database/connection");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const initialize = require("./server/utils/passport");
 
 // load config file
 dotenv.config({ path: "./config/config.env" });
 
-const Port = process.env.PORT || 3000;
+// Connect Database
+connectDB();
+
+const Port = process.env.PORT || 8080;
+
+// load passport
+initialize(passport);
 
 
 // Middlewares
@@ -30,12 +41,34 @@ app.set("views", path.resolve(__dirname, "views/pages"));
 // load body parser
 app.use(express.urlencoded({ extended: false }));
 
+// connect flash
+app.use(flash());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRETE,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
+
 
 // set global variables
 app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    res.locals.user = req.user;
+    next();
 });
+
+
+
+// passport Middlewares
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 // load static files
@@ -47,9 +80,20 @@ app.use("/img", express.static(path.resolve(__dirname, "./public/img")));
 
 
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+
+// API EndPoints
+app.use("/secure", require("./server/routes/authRoute")); // Auth Route
+app.use("/", require("./server/routes/pageRoute")); // Page Routes
+
+
+
+app.use((req, res) => {
+  res.send("Page Not Found");
 });
+
+
+
+
 
 
 
